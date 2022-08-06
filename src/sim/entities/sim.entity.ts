3dce069d8a711a05d8d8e2f1db275c0948357077
pavantamidala <pavantamidala023@gmail.com>
2022-08-06 16:38:39 +0530

    ftr:added entity columns

--- src/sim/entities/sim.entity.ts
+++ src/sim/entities/sim.entity.ts
@@ -1,1 +1,44 @@
-export class Sim {}
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+export class Sim {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  simCardNo: number;
+  @Column()
+  mobileNo: number;
+  @Column()
+  status: string;
+  @Column()
+  expiryDate: number;
+  @Column()
+  stateOfRegistration: string;
+  @Column()
+  kyc: boolean;
+  @Column()
+  telecomProvider: string;
+  @Column()
+  fullName: string;
+
+  @AfterInsert()
+  insert() {
+    console.log(`insterted user with id ${this.id}`);
+  }
+
+  @AfterRemove()
+  remove() {
+    console.log(`removed user with id ${this.id}`);
+  }
+
+  @AfterUpdate()
+  update() {
+    console.log(`updated user with id ${this.id}`);
+  }
+}
